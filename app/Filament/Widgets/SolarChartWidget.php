e401be57b6ae94db18642d19a6e8a8a78dee174c
anjun65 <hajrulkhaira@gmail.com>
2024-10-24 14:41:30 +0700

    first commit

---
+++ app/Filament/Widgets/SolarChartWidget.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Solar;
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+
+class SolarChartWidget extends ChartWidget
+{
+    protected static ?string $heading = 'Arus';
+
+    
+    
+    protected function getData(): array
+    {
+        $data = Trend::model(Solar::class)
+                ->between(
+                    start: now()->startOfDay(),
+                    end: now()->endOfDay(),
+                )
+                ->perMinute()()
+                ->average('arus');
+ 
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Arus',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
